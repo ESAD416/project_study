f9a62e82ca6d7caeef8884c4830717db976d934d
ESAD <st900266@gmail.com>
2022-10-03 18:51:09 +0800

    更名兼擴充方法

--- RPG_Windows/Assets/Scripts/AIDetector.cs
+++ RPG_Windows/Assets/Scripts/AIDetector.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Detector : MonoBehaviour
+public class AIDetector : MonoBehaviour
 {
     [Header("Detector Parameters")]
-    [SerializeField] private LayerMask targetLayer;
-    public bool targetDetected;
     [Range(.1f, 1)]
     public float detectRadius;
+    [SerializeField] private LayerMask targetLayer;
+    [SerializeField] private LayerMask visibilityLayer;
+    public bool targetDetected;
+    public bool targetVisable;
+    
+    private Transform target = null;
+    public Transform Target {
+        get => target;
+        set {
+            target = value;
+            targetVisable = false;
+        }
+    }
 
     [Header("Gizmo Parameters")]
     public Color gizmoColor = Color.blue;
@@ -27,6 +38,14 @@ public class Detector : MonoBehaviour
         if(showGizmos) {
             Gizmos.color = gizmoColor;
             Gizmos.DrawWireSphere(transform.position, detectRadius);
+        }
+    }
+
+    private void DectectTarget() {
+        if(Target == null) {
+
+        } else if(Target != null) {
+            
         }
     }
 }
